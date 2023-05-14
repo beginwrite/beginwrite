75c78436d7ea580a92006050e427875934a324e4
bebeji-nappa <masadecade@gmail.com>
2023-05-15 03:06:03 +0900

    fix

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -3,6 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('http://127.0.0.1:3000');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toBe('Hello NestJS + Next.js!');
+  await expect(page.getByText('Hello NestJS + Next.js!')).toBeTruthy();
 });

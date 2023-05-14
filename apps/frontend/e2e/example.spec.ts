e585769da5122d68c94c499bdd3d8338c703be4d
bebeji-nappa <masadecade@gmail.com>
2023-05-15 02:43:56 +0900

    アプリ上の動作をCIで検証する

--- apps/frontend/e2e/example.spec.ts
+++ apps/frontend/e2e/example.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://127.0.0.1:3000');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page.textContent("h1")).toBe('Hello NestJS + Next.js!');
 });

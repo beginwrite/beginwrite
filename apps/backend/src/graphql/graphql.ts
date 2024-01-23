6bf650f4063d7afe57a187b30d7cbcb37bde89f7
bebeji-nappa <masadecade@gmail.com>
2024-01-24 01:38:02 +0900

    fix version

---
+++ apps/backend/src/graphql/graphql.ts
@@ -0,0 +1,24 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export interface User {
+    id: string;
+    name: string;
+}
+
+export interface IMutation {
+    createUser(name: string): User | Promise<User>;
+}
+
+export interface IQuery {
+    users(): Nullable<User[]> | Promise<Nullable<User[]>>;
+}
+
+type Nullable<T> = T | null;

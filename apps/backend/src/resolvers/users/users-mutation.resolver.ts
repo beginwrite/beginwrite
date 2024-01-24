97c544ddd16771f73d7256dbd3777bf9a1556383
bebeji-nappa <masadecade@gmail.com>
2024-01-25 02:04:47 +0900

    バリデーションを強化

--- apps/backend/src/resolvers/users/users-mutation.resolver.ts
+++ apps/backend/src/resolvers/users/users-mutation.resolver.ts
@@ -10,12 +10,24 @@ export class UsersMutationResolver {
 
   @Mutation((returns) => User)
   async createUser(@Args() args: IMutationCreateUserArgs) {
+    if (!args.data.password) throw new Error('Password is required');
+    if (!args.data.email) throw new Error('Email is required');
+    if (!args.data.name) throw new Error('Name is required');
+    if (!args.data.userId) throw new Error('UserId is required');
+
     const hash = await bcrypt.hash(args.data.password, 12);
-    return this.usersRepository.createUser({
-      email: args.data.email,
-      hash,
-      name: args.data.name,
-      userId: args.data.userId,
-    });
+    return this.usersRepository
+      .createUser({
+        email: args.data.email,
+        hash,
+        name: args.data.name,
+        userId: args.data.userId,
+      })
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => {
+        throw new Error(err.message);
+      });
   }
 }

f91b3e103507acb616587aa5b882d26afa99e849
bebeji-nappa <masadecade@gmail.com>
2024-03-03 01:14:42 +0900

    型定義修正

--- apps/frontend/src/components/pages/Users/index.tsx
+++ apps/frontend/src/components/pages/Users/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import type { IUser } from '@beginwrite/app-graphql-codegen';
 import { getUsersQuery } from './gql';
-import type { GetUserQuery } from './gql';
+import type { GetUsersQuery } from './gql';
 
 const Users: React.FC = () => {
-  const { error, data } = useQuery<GetUserQuery>(getUsersQuery);
+  const { error, data } = useQuery<GetUsersQuery>(getUsersQuery);
   if (error) return null;
 
   return (

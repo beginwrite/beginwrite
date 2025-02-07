e9d6b2c4c28228ed4ba8ac45cc1f19026f714faa
bebeji-nappa <masadecade@gmail.com>
2025-02-07 22:38:09 +0900

    chore: fixup eslint config file

---
+++ apps/server/eslint.config.mjs
@@ -0,0 +1,68 @@
+import typescriptEslintEslintPlugin from "@typescript-eslint/eslint-plugin";
+import _import from "eslint-plugin-import";
+import { fixupPluginRules } from "@eslint/compat";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [{
+  ignores: ["**/.eslintrc.js", "./graphql.ts"],
+}, ...compat.extends(
+  "plugin:@typescript-eslint/recommended",
+  "plugin:prettier/recommended",
+  "prettier",
+), {
+  plugins: {
+    "@typescript-eslint": typescriptEslintEslintPlugin,
+    import: fixupPluginRules(_import),
+  },
+
+  languageOptions: {
+    parser: tsParser,
+    ecmaVersion: 5,
+    sourceType: "module",
+
+    parserOptions: {
+      project: "tsconfig.json",
+      tsconfigRootDir: __dirname,
+    },
+  },
+
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+
+    "import/order": ["error", {
+      groups: [
+        "builtin",
+        "external",
+        "internal",
+        "parent",
+        "sibling",
+        "index",
+        "object",
+          "type",
+      ],
+
+      "newlines-between": "always",
+      pathGroupsExcludedImportTypes: ["builtin"],
+      alphabetize: {
+        order: "asc",
+        caseInsensitive: true,
+      },
+    }],
+  },
+}];

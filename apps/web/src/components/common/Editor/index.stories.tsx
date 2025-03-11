57d78b3982a47e3d2b3b36812426127221508214
bebeji-nappa <masadecade@gmail.com>
2025-03-11 22:11:11 +0900

    fix(client): storybook 修正

--- apps/web/src/components/common/Editor/index.stories.tsx
+++ apps/web/src/components/common/Editor/index.stories.tsx
@@ -1,5 +1,3 @@
-import { useForm } from 'react-hook-form';
-
 import Editor from './index';
 
 import type { Meta, StoryObj } from '@storybook/react';
@@ -8,26 +6,9 @@ export default {
   title: 'Compoments/Common/Editor',
   component: Editor,
   args: {
-    onChange: async () => {},
-    value: '',
+    name: 'content',
+    value: '#aaaa\n##bbbbb\n\ntest',
   },
-  decorators: [
-    (Story) => {
-      const form = useForm({
-        defaultValues: {
-          content: '',
-        },
-      });
-
-      const { onChange } = { ...form.register('content') };
-
-      return (
-        <div style={{ width: 800 }}>
-          <Story onChnage={onChange} />
-        </div>
-      );
-    },
-  ],
 } as Meta<typeof Editor>;
 
 export const Default: StoryObj<typeof Editor> = {};

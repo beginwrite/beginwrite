4a772aa2fb5dafda2d8f324816ae3213a3edc255
bebeji-nappa <masadecade@gmail.com>
2025-03-15 05:49:02 +0900

    feat(server): DB管理ツールを drizzle ORM に移管する

---
+++ apps/server/drizzle.config.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+dotenv.config();
+
+console.log(process.env.DATABASE_URL);
+
+export default defineConfig({
+  dialect: 'mysql',
+  schema: './db/schema/*',
+  out: './db/migrations',
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  },
+});

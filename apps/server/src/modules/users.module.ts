43d627d89541cb914951354676d1afe9be9082f3
bebeji-nappa <masadecade@gmail.com>
2025-03-15 14:23:26 +0900

    fix(api): userModule 定義ミスを修正

--- apps/server/src/modules/users.module.ts
+++ apps/server/src/modules/users.module.ts
@@ -2,17 +2,25 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RedisService } from 'src/applications/services/redis.service';
 import { S3Service } from 'src/applications/services/s3.service';
+import { UpdateUserProfileAvatarUseCase } from 'src/use-cases/users/update-user-profile-avatar.use-case';
+import { UpdateUserProfileUseCase } from 'src/use-cases/users/update-user-profile.use-case';
 
 import { User } from '../domains/users/entities/users.entity';
 import { UsersRepository } from '../domains/users/repositories/users.repository';
 import { UsersMutationResolver } from '../resolvers/users/users-mutation.resolver';
 import { UsersQueryResolver } from '../resolvers/users/users-query.resolver';
+import { CreateUserUseCase } from '../use-cases/users/create-user.use-case';
+import { FindUserByIdUseCase } from '../use-cases/users/find-user-by-id.use-case';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   providers: [
     UsersRepository,
     UsersQueryResolver,
     UsersMutationResolver,
+    FindUserByIdUseCase,
+    CreateUserUseCase,
+    UpdateUserProfileUseCase,
+    UpdateUserProfileAvatarUseCase,
     S3Service,
     RedisService,
   ],
@@ -20,6 +28,10 @@ import { UsersQueryResolver } from '../resolvers/users/users-query.resolver';
     UsersRepository,
     UsersQueryResolver,
     UsersMutationResolver,
+    FindUserByIdUseCase,
+    CreateUserUseCase,
+    UpdateUserProfileUseCase,
+    UpdateUserProfileAvatarUseCase,
     S3Service,
     RedisService,
   ],
